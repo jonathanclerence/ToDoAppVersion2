a804863b38477289ae35c30e6369f3768374f73f
Jonathan <jclerence@invorg.com>
2024-10-04 16:07:57 +0530

    Initial Commit

---
+++ ToDoV2/ToDoV2App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoV2App.swift
+//  ToDoV2
+//
+//  Created by Hanan Jonathan Clerence on 2024-10-04.
+//
+
+import SwiftUI
+
+@main
+struct ToDoV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
