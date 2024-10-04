3a2f72c27c6cb00e9e3bab9bea818e04e127b302
Jonathan <jclerence@invorg.com>
2024-10-04 18:03:47 +0530

    Created To Do App version 2 which is clickable to show its done

--- ToDoV2/ContentView.swift
+++ ToDoV2/ContentView.swift
@@ -7,15 +7,98 @@
 
 import SwiftUI
 
+struct TaskItem: Identifiable, Hashable {
+    var id: String = UUID().uuidString
+    
+    var title: String
+    var description: String
+    var isDone: Bool
+}
+
 struct ContentView: View {
+    @State var tasks:[TaskItem] = []
+    @State var displayAddTask: Bool = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack {
+                List(tasks) { item in
+                    
+                    HStack {
+                        
+                        Image(systemName: item.isDone ? "circle.fill" : "circle")
+                        
+                        VStack(alignment: .leading) {
+                            Text(item.title)
+                                .font(.headline)
+                            Text(item.description)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                        }.onTapGesture {
+                            let index = tasks.firstIndex(where: {
+                                $0.id == item.id
+                            })
+                            
+                            guard let unwrappedIndex = index else { return }
+                            
+                            tasks[unwrappedIndex].isDone.toggle()
+                        }
+                    }
+                }
+                .listStyle(InsetGroupedListStyle())
+            }
+            .navigationTitle("My Tasks")
+            .toolbar{
+                ToolbarItem(placement: .topBarTrailing){
+                    Button("", systemImage: "plus") {
+                        displayAddTask.toggle()
+                    }
+                }
+            }
+            .sheet(isPresented: $displayAddTask) {
+                AddTask(tasks: $tasks)
+            }
         }
-        .padding()
+        
+    }
+}
+
+struct AddTask:View {
+    
+    @Binding var tasks:[TaskItem]
+    @State var newtask: TaskItem = TaskItem(title: "" ,description: "", isDone: false)
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        NavigationStack{
+            Form {
+                VStack {
+                    TextField("Enter a task", text: $newtask.title)
+                    TextField("Enter a description", text: $newtask.description)
+                    
+                }
+                .navigationTitle("New Task")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar{
+                    ToolbarItem(placement: .topBarTrailing){
+                        Button("Add") {
+                            tasks.append(newtask)
+                            dismiss()
+                        }
+                    }
+                    ToolbarItem(placement: .topBarLeading){
+                        Button("Cancel") {
+                            dismiss()
+                        }
+                        .tint(.red)
+                    }
+                }
+            }
+            
+
+        }
+        
     }
 }
 
